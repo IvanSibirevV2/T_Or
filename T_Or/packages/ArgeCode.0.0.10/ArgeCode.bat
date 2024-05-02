0436cb99111cf5c750181a24d21a0d754dd483d8
йцу <>
2024-05-02 13:17:51 +0300

    AutoCommit_2 мая 2024 г. 13:17:51_SibNout2023

--- T_Or/packages/ArgeCode.0.0.10/ArgeCode.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-echo %cd%
-git clone https://github.com/IvanSibirevV2/ArgeCode.git
-del %0
--- T_Or/packages/NuGet_Forge.0.0.14/NuGet_Forge/LocalFileUpDater.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd %~dp0
-echo %cd%
-del "КакойТоФайл"
-copy "КакойТоФайл" "Куда копипастить"
-PAUSE
-exit /b
